81e173339f156f0c4f8097fbec68c1377c4f0b9c
longvt93 <vutuanlongdhcn@gmail.com>
2023-10-15 22:59:34 +0700

    Update lọc trang category product

--- sidebar.php
+++ sidebar.php
@@ -34,59 +34,6 @@
 	</ul>
 </div>
 
-<!-- sản phẩm bán chạy -->
-<div class="side-block product-item">
-	<h3 class="side-title">SẢN PHẨM BÁN CHẠY</h3>
-	<div class="row no-gutters">
-		<?php
-		$args = [
-			'post_type' => 'product',
-			'showposts' => '5',
-			'order'     => 'DESC',
-			'orderby'   => 'date',
-			'tax_query' => [
-				[
-					'taxonomy' => 'product_cat',
-					'field'    => 'term_id',
-					'terms'    => 42,
-				],
-			],
-		];
-		$news = new WP_Query( $args );
-		?>
-		<?php if ( $news->have_posts() ) : ?>
-			<?php
-			while ( $news->have_posts() ) :
-				$news->the_post();
-				global $product;
-				?>
-				<div class="col-sm-6 col-md-12">
-					<div class="product-item text-center">
-						<div class="row no-gutter-s">
-							<a href="<?php the_permalink() ?>">
-								<div class="col-sm-4 col-md-3">
-									<?php the_post_thumbnail( 'thumbnail', array( 'class' => 'img-responsive center-block' ) ); ?>
-								</div>
-							</a>
-							<div class="col-sm-8 col-md-9">
-								<a href="<?php the_permalink() ?>">
-									<p class="product_name"><?php the_title() ?></p>
-								</a>
-								<p class="side-price"><?php echo $product->get_price_html(); ?></p>
-							</div>
-						</div>
-					</div>
-				</div>
-			<?php endwhile; ?>
-		<?php else : ?>
-			<?php get_template_part( 'template-parts/content', 'none' ); ?>
-			<?php
-		endif;
-		wp_reset_query();
-		?>
-	</div>
-</div>
-
 <!-- Bộ lọc -->
 <form name="form_filter" id="form_filter" action="" method="GET">
 	<div class="side-block">
@@ -109,7 +56,13 @@
 					$active        = in_array( $term->slug, $product_brand );
 					?>
 					<li class="item <?= $active ? 'active' : '' ?>">
-						<input id="data-brand-p<?= esc_attr( $key ) ?>" type="checkbox" name="product_brand[]" value="<?= esc_attr( $term->slug ) ?>">
+						<input
+							id="data-brand-p<?= esc_attr( $key ) ?>"
+							type="checkbox"
+							name="product_brand[]"
+							value="<?= esc_attr( $term->slug ) ?>"
+							<?= $active ? 'checked' : '' ?>
+						>
 						<label for="data-brand-p<?= esc_attr( $key ) ?>">
 							<?php echo esc_html( $term->name ); ?>
 						</label>
@@ -139,7 +92,13 @@
 					$active      = in_array( $term->slug, $price_range );
 					?>
 					<li class="item <?= $active ? 'active' : '' ?>">
-						<input id="data-price-range-p<?= esc_attr( $key ) ?>" type="checkbox" name="price_range[]" value="<?= esc_attr( $term->slug ) ?>">
+						<input
+							id="data-price-range-p<?= esc_attr( $key ) ?>"
+							type="checkbox"
+							name="price_range[]"
+							value="<?= esc_attr( $term->slug ) ?>"
+							<?= $active ? 'checked' : '' ?>
+						>
 						<label for="data-price-range-p<?= esc_attr( $key ) ?>">
 							<?php echo esc_html( $term->name ); ?>
 						</label>
@@ -151,7 +110,7 @@
 
 	<div class="side-block">
 		<h3 class="side-title">Lọc theo màu sắc</h3>
-		<div class="filter_group-content filter-color">
+		<div class="filter_group-content filter-color filter-list">
 			<?php
 			$terms = get_terms( [
 				'taxonomy'   => 'pa_color',
@@ -169,7 +128,12 @@
 				?>
 				<label class="item <?= $active ? 'active' : '' ?>">
 					<div class="color" style="background-color: <?= esc_attr( $term->slug ) ?>" title="<?= esc_attr( $term->name ) ?>" data-name="<?= esc_attr( $term->name ) ?>"></div>
-					<input type="checkbox" name="balo_color[]" value="<?= esc_attr( $term->slug ) ?>">
+					<input
+						type="checkbox"
+						name="balo_color[]"
+						value="<?= esc_attr( $term->slug ) ?>"
+						<?= $active ? 'checked' : '' ?>
+					>
 				</label>
 			<?php endforeach; ?>
 		</div>
@@ -195,7 +159,13 @@
 					$active       = in_array( $term->slug, $product_size );
 					?>
 					<li class="item <?= $active ? 'active' : '' ?>">
-						<input id="data-size-p<?= esc_attr( $key ) ?>" type="checkbox" name="balo_size[]" value="<?= esc_attr( $term->slug ) ?>">
+						<input
+							id="data-size-p<?= esc_attr( $key ) ?>"
+							type="checkbox"
+							name="balo_size[]"
+							value="<?= esc_attr( $term->slug ) ?>"
+							<?= $active ? 'checked' : '' ?>
+						>
 						<label for="data-size-p<?= esc_attr( $key ) ?>">
 							<?php echo esc_html( $term->name ); ?>
 						</label>
@@ -203,5 +173,8 @@
 				<?php endforeach; ?>
 			</ul>
 		</div>
+	</div>
+	<div class="filter-button">
+		<span class="btn-filter-readmore">Xem kết quả</span>
 	</div>
 </form>
