5463279df21efdd058bff33e2bfd69ad353225ec
longvt93 <vutuanlongdhcn@gmail.com>
2023-11-23 23:55:01 +0700

    Update trang blog

--- archive.php
+++ archive.php
@@ -11,92 +11,115 @@ get_header();?>
 	<div class="page__section">
 		<div class="container">
 			<nav class="breadcrumbs ">
-		        <?php if (function_exists('rank_math_the_breadcrumbs')) rank_math_the_breadcrumbs(); ?>
+				<?php
+				if ( function_exists( 'rank_math_the_breadcrumbs' ) ) {
+					rank_math_the_breadcrumbs();}
+				?>
 			</nav>
 			<div class="archive-content">
 				<div class="news-main">
-					<div class="row">			
-						<div class="col-md-8">
+					<div class="row">
+						<div class="col-md-9">
 							<h1 class="title-cat">
 								<?php single_cat_title(); ?>
 							</h1>
-							<hr/>
-							<?php if ( have_posts() ): ?>
-							    <?php while (have_posts()) : the_post(); global $post; ?>
-										<?php get_template_part('template-parts/content','news' ); ?>
-							    <?php endwhile; ?>
-							<?php else : ?>
-								<?php get_template_part( 'template-parts/content', 'none' ); ?>
-							<?php endif; wp_reset_query();?>
+							<div class="row blog-posts">
+								<?php if ( have_posts() ) : ?>
+									<?php
+									while ( have_posts() ) :
+										the_post();
+										global $post;
+										?>
+										<?php get_template_part( 'template-parts/content', 'news' ); ?>
+									<?php endwhile; ?>
+								<?php else : ?>
+									<?php get_template_part( 'template-parts/content', 'none' ); ?>
+									<?php
+								endif;
+								wp_reset_query();
+								?>
+							</div>
 						</div>
-						<div class="col-md-4">
+						<div class="col-md-3">
 							<div class="news-side">
 								<div class="sidebar_widget_content">
 									<div class="side-block product-item">
 										<h3 class="side-title-news">TIN TỨC NỔI BẬT</h3>
 										<div class="fe-pro">
-											<?php  $args = array(			
-												'post_type' => 'post',					
-												'showposts' => '5',					
-												'order' => 'DESC', 					
-												'orderby' => 'date',					
-												                        				
-											); 					
-											$news = new WP_Query($args); ?>					
-											<?php  if ($news->have_posts()) : ?>					
-												<?php while ($news->have_posts()) : $news->the_post(); global $product;?>
-													<div class="row no-gutters">					
+											<?php
+											$args = [
+												'post_type' => 'post',
+												'showposts' => '5',
+												'order'   => 'DESC',
+												'orderby' => 'date',
+											];
+											$news = new WP_Query( $args );
+											?>
+											<?php if ( $news->have_posts() ) : ?>
+												<?php
+												while ( $news->have_posts() ) :
+													$news->the_post();
+													global $product;
+													?>
+													<div class="row no-gutters">
 														<div class="col-md-3">
-															<a href="<?php the_permalink();?>">
-																<?php the_post_thumbnail( 'zw-news',array( 'class' => 'img-responsive center-block' ) ); ?>
+															<a href="<?php the_permalink(); ?>">
+																<?php the_post_thumbnail( 'zw-news', [ 'class' => 'img-responsive center-block' ] ); ?>
 															</a>
 														</div>
 														<div class="col-md-9">
 															<div class="meta-text">
-																<a href="<?php the_permalink();?>">
+																<a href="<?php the_permalink(); ?>">
 																	<?php the_title( '<p class="entry-title">', '</p>' ); ?>
 																</a>
-																<?php
-																echo 	'<div class="entry-meta">
-																<p class="meta__date"><i class="fa fa-calendar-check-o" aria-hidden="true"></i> '.
-																get_the_time("d/m/Y")
-																.'</p>
-																</div>';
-																?>
+																<div class="entry-meta">
+																	<p class="meta__date">
+																		<i class="fa fa-calendar-check-o" aria-hidden="true"></i>
+																		<?= get_the_time( 'd/m/Y' ) ?>
+																	</p>
+																</div>
 															</div>
-															
 														</div>
-													</div>						
-												<?php endwhile; ?>					
-											<?php else : ?>					
-												<?php get_template_part( 'template-parts/content', 'none' ); ?>				
-											<?php endif; wp_reset_query();?>
+													</div>
+												<?php endwhile; ?>
+											<?php else : ?>
+												<?php get_template_part( 'template-parts/content', 'none' ); ?>
+												<?php
+											endif;
+											wp_reset_query();
+											?>
 										</div>
 									</div>
 								</div>
 								<div class="side-block product-item">
 									<h3 class="side-title">SẢN PHẨM NỔI BẬT</h3>
 									<div class="fe-pro">
-										<?php  $args = array(			
-											'post_type' => 'product',					
-											'showposts' => '5',					
-											'order' => 'DESC', 					
-											'orderby' => 'date',					
-											'tax_query' => array(					
-												array(			
-													'taxonomy' => 'product_cat',		
-													'field'    => 'term_id',		
-													'terms'    => 42,		
-												),			
-											),                          				
-										); 					
-										$news = new WP_Query($args); ?>					
-										<?php  if ($news->have_posts()) : ?>					
-											<?php while ($news->have_posts()) : $news->the_post(); global $product;?>	
+										<?php
+										$args = [
+											'post_type' => 'product',
+											'showposts' => '5',
+											'order'     => 'DESC',
+											'orderby'   => 'date',
+											'tax_query' => [
+												[
+													'taxonomy' => 'product_cat',
+													'field'    => 'term_id',
+													'terms'    => 42,
+												],
+											],
+										];
+										$news = new WP_Query( $args );
+										?>
+										<?php if ( $news->have_posts() ) : ?>
+											<?php
+											while ( $news->have_posts() ) :
+												$news->the_post();
+												global $product;
+												?>
 												<div class="row no-gutters">
-													<a href="<?php the_permalink() ?>">		
+													<a href="<?php the_permalink() ?>">
 														<div class="col-md-3">
-															<?php the_post_thumbnail( 'thumbnail',array( 'class' => 'img-responsive center-block' ) ); ?>
+															<?php the_post_thumbnail( 'thumbnail', [ 'class' => 'img-responsive center-block' ] ); ?>
 														</div>
 													</a>
 													<div class="col-md-9">
@@ -104,18 +127,21 @@ get_header();?>
 															<p class="product_name"><?php the_title() ?></p>
 														</a>
 														<p class="side-price"><?php echo $product->get_price_html(); ?></p>
-													</div>	
-												</div>					
-											<?php endwhile; ?>					
-										<?php else : ?>					
-											<?php get_template_part( 'template-parts/content', 'none' ); ?>				
-										<?php endif; wp_reset_query();?>
+													</div>
+												</div>
+											<?php endwhile; ?>
+										<?php else : ?>
+											<?php get_template_part( 'template-parts/content', 'none' ); ?>
+											<?php
+										endif;
+										wp_reset_query();
+										?>
 									</div>
 								</div>
 							</div>
 
-				        </div>
-				    </div>
+						</div>
+					</div>
 				</div>
 
 				<div class="nav-page">
@@ -124,18 +150,5 @@ get_header();?>
 			</div>
 		</div>
 	</div>
-
-<?php get_footer(); ?><?php
-/**
- * archive file
- * @package    WordPress
- * @subpackage ZW
- * @author     Viet Loc
- * @version    1.0
- * @link       www.zoomworld.vn
- */
-get_header();?>
-
-<!-- code here -->
 
 <?php get_footer(); ?>
--- template-parts/content-news.php
+++ template-parts/content-news.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 /**
  * The template parts
  *
@@ -6,21 +6,21 @@
  * @subpackage zoomworld
  */
 ?>
-<article class="news__item">
-	<div class="row">
-		<div class="col-md-4">
-			<div class="news__thumb block__thumb--style transition--thumbnail">
-				<a href="<?php the_permalink(); ?>">
-					<?php the_post_thumbnail( 'zw-news',array( 'class' => 'img-responsive center-block' ) ); ?>
-				</a>
-			</div>
-		</div>
-		<div class="col-md-8">
-			<div class="news-meta">
-				<h3 class="news-title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h3>
-				<?php the_date('d-m-Y', '<p>', '</p>'); ?>
-				<p class="news-excerpt hidden-xs"><?php echo zw_limit_words(get_the_excerpt(), '45'); ?></p>
-			</div>
+<article class="news__item col-lg-4 col-sm-6 col-12">
+	<div class="news__thumb block__thumb--style transition--thumbnail">
+		<a href="<?php the_permalink(); ?>">
+			<?php the_post_thumbnail( 'zw-news', array( 'class' => 'img-responsive center-block' ) ); ?>
+		</a>
+	</div>
+
+	<div class="news-detail">
+		<h3 class="news-title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h3>
+		<p class="news-excerpt hidden-xs"><?php echo zw_limit_words( get_the_excerpt(), '18' ); ?></p>
+		<div class="news-meta">
+			<span class="author">Đăng bởi: <?= get_the_author(); ?></span>
+			<span class="date">
+				<?= get_the_date( 'j M, Y' ); ?>
+			</span>
 		</div>
 	</div>
 </article>
